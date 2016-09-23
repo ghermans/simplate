89b77fcb94249ba8bea5c4fe973960787a249271
mikail <mikail.aziev@gmail.com>
2016-09-23 16:16:42 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function(){
-	//
+    //
 });
 
 gulp.task('sass', function () {
-	return gulp.src('css/scss/*.scss')
-	  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	  .pipe(gulp.dest('css'));
+    return gulp.src('css/scss/*.scss')
+        .pipe(sass({
+            // outputStyle: 'compressed'
+        })
+        .on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('css/main.scss', ['sass']);
+    gulp.watch('css/scss/*.scss', ['sass']);
 });
