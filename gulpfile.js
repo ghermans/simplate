fdad8e3dc63bf58d576ec87cd6213e4426905626
Mikail <mikail@ant.ms>
2017-01-15 17:30:44 +0300

    change file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var merge = require('merge-stream');
 
 gulp.task('default', function(){
     //
 });
 
+gulp.task('assets', function(){
+    var jquery = gulp.src('node_modules/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('public/js'));
+        
+    var normalize = gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(rename('_normalize.scss'))
+        .pipe(gulp.dest('assets/sass'));
+
+    return merge(jquery, normalize);
+});
+
 gulp.task('sass', function () {
-    return gulp.src('css/scss/*.scss')
+    return gulp.src('assets/sass/*.scss')
         .pipe(sass({
             // outputStyle: 'compressed'
         })
         .on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(rename('bundle.css'))
+        .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('css/scss/*.scss', ['sass']);
+    gulp.watch('assets/sass/*.scss', ['sass']);
 });
